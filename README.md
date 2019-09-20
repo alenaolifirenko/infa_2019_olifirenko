47e2853e4709aa0922b2f4ac1339f835b6bc9c66
Olifirenko Elena <alenaolifirenko@rambler.ru>
2019-09-20 15:09:54 +0300

    new file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # infa_2019_olifirenko
+it's a test
---
+++ lab3/picture1.py
@@ -0,0 +1,18 @@
+from graph import *
+
+penColor('black')
+penSize(2)
+brushColor('yellow')
+circle(200, 150, 100)
+brushColor('black')
+rectangle(150, 200, 250, 220)
+polygon([(100, 45), (180, 120), (175, 125), (90, 50), (100, 45)])
+polygon([(290, 50), (220, 120), (230, 123), (300, 53),(290, 50)])
+brushColor('red')
+circle(150, 130, 18)
+circle(250, 130, 18)
+brushColor('black')
+circle(150, 130, 13)
+circle(250, 130, 13)
+
+run()
